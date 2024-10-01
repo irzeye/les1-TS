5b9193583bd36499cd3a52244cac4ada4733a7dd
Irina Zaykina <irzeye@mail.ru>
2024-10-01 19:22:22 +0000

    step4

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,5 +1,37 @@
-export function Product(){
+import { useState } from "react"
+import { IProduct } from "../models"
+
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product({ product }: ProductProps) {
+    const [details, setDetails] = useState(false)
     return (
-        <div>Product!</div>
+        <div
+        className="border py-2 px-4 rounded flex flex-col items-center mb-2"
+        >
+            <img src={product.image} className="w-1/6" alt={product.Title} />
+           <p>{ product.Title }</p>
+           <p className="font-bold">{product.price}</p>
+           <button 
+           className="py-2 px-4 border bg-yellow-400"
+           onClick={()=>setDetails(true)}
+           >
+            Show Details
+            </button>
+
+            <button 
+           className="py-2 px-4 border bg-blue-400"
+           onClick={()=>setDetails(false)}
+           >
+            Hide Details
+            </button>
+            
+            {details && <div>
+                <p>{ product.description }</p>
+            </div>}
+
+            </div>
     )
 }
