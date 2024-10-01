c3e6fffcb105bc3cc8a69859635551578302b27f
Irina Zaykina <irzeye@mail.ru>
2024-10-01 16:52:32 +0000

    step2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {createElement as e, useState} from 'react';
 
 function App() {
-  return (
-	  <h1>Hello React!!!</h1>
-  );
+const [count, setCount] = useState(0)
+
+return e('div', {className: 'container'}, [
+	e('h1', {className: 'font-bold', key: 1}, `Test JSX ${count}`),
+	e('button', {
+		className: 'py-2 px-4 border', 
+		key: 2, 
+		onClick: ()=> setCount(count+1)}, 
+		'Click me!')
+])
 }
 
 export default App;
