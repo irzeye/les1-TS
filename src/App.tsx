12e9543292c070f7556f20578527f7375df22671
Irina Zaykina <irzeye@mail.ru>
2024-10-01 19:57:26 +0000

    step6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import {Product} from './components/product'
+import {Product} from './components/Product'
 import {products} from './data/products'
 
 function App() {
 return (
 	<div className="container mx-auto max-w-2xl pt-5">
-		<Product product={products[0]} />
-		<Product product={products[1]} />
+
+		{ products.map(product => <Product product={product} key={product.id} />)}
+
+		{/* <Product product={products[0]} /> */}
+		{/* <Product product={products[1]} /> */}
 	</div>
 )
 
