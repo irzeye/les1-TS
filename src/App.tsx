02914d505b257d2809173fa787c8e2e89a8e85e2
Irina Zaykina <irzeye@mail.ru>
2024-10-02 12:48:56 +0000

    step7. загрузка с сервера и визуализация загрузки страницы

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'
 import {Product} from './components/Product'
-import {products} from './data/products'
+// import {products} from './data/products'
+import axios from 'axios'
+import { IProduct } from './models'
 
 function App() {
+	const [products, setProducts] = useState<IProduct[]>([])
+	const [loading, setLoading] = useState(false) 
+
+	async function fetchProducts() {
+		setLoading(true)
+		const respons = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+	setProducts(respons.data)
+	setLoading(false)
+}
+
+useEffect( () => {
+	fetchProducts()
+}, [])
+
 return (
 	<div className="container mx-auto max-w-2xl pt-5">
-
-		{ products.map(product => <Product product={product} key={product.id} />)}
-
-		{/* <Product product={products[0]} /> */}
-		{/* <Product product={products[1]} /> */}
+		{loading && <p className="text-center">Loading...</p>}
+		{ products.map(product => <Product product={product} key={product.id} />) }
 	</div>
 )
 
