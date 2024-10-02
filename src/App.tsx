686b05e2abe17fc703b8c645e4ebf625a26b4f92
Irina Zaykina <irzeye@mail.ru>
2024-10-02 13:41:15 +0000

    step8. вывод ошибки при проблемах с загрузкой

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React, {useEffect, useState} from 'react'
 import {Product} from './components/Product'
 // import {products} from './data/products'
-import axios from 'axios'
+import axios, { Axios, AxiosError } from 'axios'
 import { IProduct } from './models'
 
 function App() {
 	const [products, setProducts] = useState<IProduct[]>([])
 	const [loading, setLoading] = useState(false) 
+	const [error, setError] = useState('')
 
 	async function fetchProducts() {
-		setLoading(true)
+		try {
+	setError('')	
+	setLoading(true)
 		const respons = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
 	setProducts(respons.data)
 	setLoading(false)
+		} catch (e: unknown) {
+			const error = e as AxiosError
+	setLoading(false)
+	setError(error.message)	
+	}
 }
 
 useEffect( () => {
@@ -22,6 +30,8 @@ useEffect( () => {
 return (
 	<div className="container mx-auto max-w-2xl pt-5">
 		{loading && <p className="text-center">Loading...</p>}
+		{error && <p className="text-center text-red-600">{ error }</p>}
+		
 		{ products.map(product => <Product product={product} key={product.id} />) }
 	</div>
 )
