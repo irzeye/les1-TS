f9157a20c96628e4746d2477b419860e28d0d622
Irina Zaykina <irzeye@mail.ru>
2024-10-02 16:19:26 +0000

    step9. вынос функционала в отдельный файл

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import React, {useEffect, useState} from 'react'
 import {Product} from './components/Product'
-// import {products} from './data/products'
-import axios, { Axios, AxiosError } from 'axios'
-import { IProduct } from './models'
+import { useProducts } from './hooks/products';
 
 function App() {
-	const [products, setProducts] = useState<IProduct[]>([])
-	const [loading, setLoading] = useState(false) 
-	const [error, setError] = useState('')
-
-	async function fetchProducts() {
-		try {
-	setError('')	
-	setLoading(true)
-		const respons = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-	setProducts(respons.data)
-	setLoading(false)
-		} catch (e: unknown) {
-			const error = e as AxiosError
-	setLoading(false)
-	setError(error.message)	
-	}
-}
-
-useEffect( () => {
-	fetchProducts()
-}, [])
+const {loading, error, products} = useProducts()
+	
 
 return (
 	<div className="container mx-auto max-w-2xl pt-5">
---
+++ src/hooks/products.ts
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react'
+import {IProduct} from '../models'
+import axios, { Axios, AxiosError } from 'axios'
+
+export function useProducts() {
+    const [products, setProducts] = useState<IProduct[]>([])
+	const [loading, setLoading] = useState(false) 
+	const [error, setError] = useState('')
+
+	async function fetchProducts() {
+		try {
+	setError('')	
+	setLoading(true)
+		const respons = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+	setProducts(respons.data)
+	setLoading(false)
+		} catch (e: unknown) {
+			const error = e as AxiosError
+	setLoading(false)
+	setError(error.message)	
+	}
+}
+
+useEffect( () => {
+	fetchProducts()
+}, [])
+
+return { products, error, loading }
+
+}
